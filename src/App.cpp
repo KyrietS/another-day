9f004cfe067e49ed525948a0241de38639cfca74
Kyriet <kyriet.sebastian@gmail.com>
2025-01-11 23:11:39 +0100

    Fix: SDL prevents app from exiting on SIGINT
    When wxWidgets/wxWidgets#25067 is merged this workaround can be removed

--- src/App.cpp
+++ src/App.cpp
@@ -1,11 +1,20 @@
 #include "pch.hpp"
 #include "App.hpp"
 #include "MainWindow.hpp"
+#include <csignal>
+
+extern "C" void OnSigInt(int sig)
+{
+	exit(sig);
+}
 
 namespace minutea
 {
 	bool App::OnInit()
 	{
+		// Fix needed until https://github.com/wxWidgets/wxWidgets/pull/25067 is merged.
+		signal(SIGINT, OnSigInt);
+
 		if (!wxApp::OnInit())
 			return false;
 
